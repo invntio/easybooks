84ca3417630d5cdf53a5eda7c9b94bc921553317
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-03-15 13:14:46 -0400

    Configuring main.ts file for security
    * Configured first main.ts security settings
    * Added dummy code to test XSS
    * Added redundant code to test Snyk and SonarLint/SonarCloud

---
+++ backend/config/index.ts
@@ -0,0 +1,2 @@
+export { default as envConfig } from './envconfig';
+export { default as ormConfig } from './ormconfig';
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envConfig, ormConfig } from 'config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfig, ormConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
