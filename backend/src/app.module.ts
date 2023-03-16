31eabe296c3e427448940105a35d93162d8c9bd3
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-03-16 09:39:11 -0400

    Organize dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { envConfig, ormConfig } from 'config';
+import { envConfig, ormConfig } from './config';
 
 @Module({
   imports: [
