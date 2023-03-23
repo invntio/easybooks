0042e74fa388ce5c97fd8dcfcbe58266eecf5d24
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-03-23 10:49:49 -0400

    Add database config to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { envConfig, ormConfig } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [envConfig, ormConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+      inject: [ConfigService],
+    }),
+    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
