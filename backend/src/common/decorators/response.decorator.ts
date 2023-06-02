3cf8a37beb75ea8ae0dd2a4a4f485d618710a25c
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-06-02 16:07:17 -0400

    Add parameters to Response Message Decorator

--- backend/src/common/decorators/response.decorator.ts
+++ backend/src/common/decorators/response.decorator.ts
@@ -1,5 +1,10 @@
 import { SetMetadata } from '@nestjs/common';
 
+export interface ResponseMessageOptions {
+  okMessage: string;
+  emptyArrayMessage?: string;
+}
+
 export const ResponseMessageKey = 'RESPONSE_MESSAGE_KEY';
-export const ResponseMessage = (message: string) =>
-  SetMetadata(ResponseMessageKey, message);
+export const ResponseMessage = (options: ResponseMessageOptions) =>
+  SetMetadata(ResponseMessageKey, options);
